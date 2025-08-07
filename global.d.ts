8d295f9df46f20183db8e4e7d4b669b9fbf58ef4
StageDog <aksanajw845@gmail.com>
2025-08-07 17:19:01 +0800

    chore: 补充 zod.infer 等类型全局可用

--- global.d.ts
+++ global.d.ts
@@ -11,5 +11,9 @@ declare module '*.css' {
   export default content;
 }
 
-declare const YAML: typeof import('yaml');
 declare const z: typeof import('zod');
+declare namespace z {
+  export type infer<T> = import('zod').infer<T>;
+  export type input<T> = import('zod').input<T>;
+  export type output<T> = import('zod').output<T>;
+}
