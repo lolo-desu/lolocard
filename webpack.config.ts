b715417355d2e944a0f69cd09077775e4591ced0
StageDog <aksanajw845@gmail.com>
2025-08-08 17:19:32 +0800

    chore: 修复 .css 导入

--- webpack.config.ts
+++ webpack.config.ts
@@ -228,10 +228,11 @@ function parse_configuration(entry: Entry): (_env: any, argv: any) => webpack.Co
         }
 
         if (
-          request.startsWith('http') ||
-          request.startsWith('@') ||
+          request.startsWith('-') ||
           request.startsWith('.') ||
           request.startsWith('/') ||
+          request.startsWith('@') ||
+          request.startsWith('http') ||
           path.isAbsolute(request) ||
           fs.existsSync(path.join(context, request)) ||
           fs.existsSync(request)
