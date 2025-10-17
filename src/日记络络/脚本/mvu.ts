f0a7458211f1a078f680c7b0d00b7677666aa735
StageDog <aksanajw845@gmail.com>
2025-10-17 21:52:41 +0800

    [日记络络] 使用新版 mvu 脚本

--- src/日记络络/脚本/mvu.ts
+++ src/日记络络/脚本/mvu.ts
@@ -1,1 +1,2 @@
-import 'https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@master/artifact/bundle.js';
+// eslint-disable-next-line import-x/no-unresolved
+import 'https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js';
