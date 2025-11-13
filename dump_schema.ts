9eca8e92502435c1fa9e0b5db419b647bdb2f9f6
StageDog <aksanajw845@gmail.com>
2025-11-13 11:25:33 +0800

    变量 schema 文件有效性检验

--- dump_schema.ts
+++ dump_schema.ts
@@ -1,13 +1,16 @@
 /* eslint-disable */
 // @ts-nocheck
+import _ from 'lodash';
 import fs from 'node:fs';
 import path from 'node:path';
 import { z } from 'zod';
 
 fs.globSync('src/*/schema.ts').forEach(async schema_file => {
-  const { Schema } = await import(path.resolve(import.meta.dirname, schema_file));
-  fs.writeFileSync(
-    path.join(path.dirname(schema_file), 'schema.json'),
-    JSON.stringify(z.toJSONSchema(Schema, { io: 'input', reused: 'ref' }), null, 2),
-  );
+  const module = await import(path.resolve(import.meta.dirname, schema_file));
+  if (_.has(module, 'Schema')) {
+    fs.writeFileSync(
+      path.join(path.dirname(schema_file), 'schema.json'),
+      JSON.stringify(z.toJSONSchema(_.get(module, 'Schema'), { io: 'input', reused: 'ref' }), null, 2),
+    );
+  }
 });
