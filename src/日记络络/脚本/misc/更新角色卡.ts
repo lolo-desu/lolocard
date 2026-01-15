5fbd059c05cbf419e5fdaf0f98c3ff7d68753f19
StageDog <aksanajw845@gmail.com>
2026-01-15 17:55:18 +0800

    [日记络络] feat: 取消对低版本酒馆助手的自动更新支持 + feat: 强调CG时不要有立绘

--- src/日记络络/脚本/misc/更新角色卡.ts
+++ src/日记络络/脚本/misc/更新角色卡.ts
@@ -16,23 +16,16 @@ export async function checkUpdate() {
   ) {
     return;
   }
+  if (compare(getTavernVersion(), '4.3.18', '<')) {
+    toastr.warning('检测到角色卡有更新，但酒馆助手版本过低，无法自动更新', '白化蓝染的日记本')
+    return;
+  }
   await importRawCharacter(
     CHARACTER_NAME,
     await fetch('https://testingcf.jsdelivr.net/gh/lolo-desu/lolocard/src/日记络络/白化蓝染的日记本.png').then(
       response => response.blob(),
     ),
   );
-  if (compare(getTavernVersion(), '4.3.18', '<')) {
-    SillyTavern.saveWorldInfo(
-      WORLDBOOK_NAME,
-      JSON.parse(
-        await fetch('https://testingcf.jsdelivr.net/gh/lolo-desu/lolocard/src/日记络络/世界书/日记络络.json').then(
-          response => response.text(),
-        ),
-      ),
-      true,
-    );
-  }
   toastr.success('角色卡自动更新成功, 准备刷新页面以生效...', '白化蓝染的日记本');
   setTimeout(() => triggerSlash('/reload-page'), 3000);
 }
