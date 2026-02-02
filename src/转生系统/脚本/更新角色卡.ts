0b35061c147fbeb6428be1dc07e2218b86d28f28
StageDog <aksanajw845@gmail.com>
2026-02-02 23:04:58 +0800

    fix

--- src/转生系统/脚本/更新角色卡.ts
+++ src/转生系统/脚本/更新角色卡.ts
@@ -2,7 +2,7 @@ import { compare } from 'compare-versions';
 
 const CHARACTER_NAME = '作为异世界的系统' as const;
 
-export async function checkUpdate() {
+async function checkUpdate() {
   const current_version = (await getCharacter(CHARACTER_NAME)).version.trim() || '0.0.0';
   const latest_version = await fetch('https://testingcf.jsdelivr.net/gh/lolo-desu/lolocard/src/转生系统/index.yaml')
     .then(response => response.text())
@@ -19,3 +19,5 @@ export async function checkUpdate() {
   );
   toastr.success(`角色卡已自动更新到 'v${latest_version}'`, CHARACTER_NAME);
 }
+
+$(checkUpdate);
